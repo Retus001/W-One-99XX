0f00fa562d600defe168c55e925dda8b15b003b6
Galaxyben <galaxyben1@gmail.com>
2017-12-04 16:40:45 -0600

    MartCart Movement (sin balanceo de stats)

--- Assets/Scripts/MartCarMovement.cs
+++ Assets/Scripts/MartCarMovement.cs
@@ -6,14 +6,21 @@ public class MartCarMovement : MonoBehaviour {
 
 	Rigidbody rigi;
 	public float Pawah;
+	public float MaxSpeed;
+	public float RotationSpeed;
+	public float RotationBreaks;
+	public float Frenos;
 	//TurbinaR
-	float RT_Force = 0;
+	float RT_Force = 0; // Right Translation Force
+	float RR_Force = 0; //Right Rotation Force
 	public KeyCode RT_Forward;   	//Tecla configurable para avanzar
 	public KeyCode RT_Backguard;	//y para retroceder (la turbina)
 	//TurbinaL
-	float LT_Force = 0;
+	float LT_Force = 0; //Left Translation Force
+	float LR_Force = 0; //Left Rotation Force
 	public KeyCode LT_Forward;
 	public KeyCode LT_Backguard;
+	public Vector3 test;
 
 	// Use this for initialization
 	void Start () {
@@ -22,22 +29,39 @@ public class MartCarMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (RT_Forward)) {
+		if (Input.GetKey (RT_Forward)) {
 			RT_Force = 1;
-		} else if (Input.GetKeyDown (RT_Backguard)) {
+
+		} else if (Input.GetKey (RT_Backguard)) {
 			RT_Force = -1;
 		} else {
 			RT_Force = 0;
 		}
-		if (Input.GetKeyDown (LT_Forward)) {
+		if (Input.GetKey (LT_Forward)) {
 			LT_Force = 1;
-		} else if (Input.GetKeyDown (LT_Backguard)) {
+		} else if (Input.GetKey (LT_Backguard)) {
 			LT_Force = -1;
 		} else {
 			LT_Force = 0;
 		}
+			
+		if (rigi.velocity.magnitude < MaxSpeed) {
+			rigi.velocity += gameObject.transform.forward * (LT_Force + RT_Force) * Pawah;
+		}
+		//gameObject.transform.Rotate (Vector3.up * (LT_Force + RT_Force) * RotationSpeed);
+		rigi.angularVelocity += gameObject.transform.up * (-LT_Force + RT_Force) * RotationSpeed;
 
-		rigi.velocity = Vector3.forward * (LT_Force + RT_Force) * Pawah;
+		//Friccion del aire ...?
+		rigi.velocity -= Frenos*rigi.velocity;
+		if (rigi.velocity.magnitude < 0)
+			rigi.velocity = Vector3.zero;
+
+		test = rigi.angularVelocity;
+		rigi.angularVelocity -= RotationBreaks*rigi.angularVelocity;
+		if (rigi.angularVelocity.magnitude <= 0) {
+			rigi.angularVelocity = Vector3.zero;
+		}
+
 
 	}
 }
