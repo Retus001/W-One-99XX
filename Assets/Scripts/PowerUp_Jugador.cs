89f613effba1e95dba0c2991064c0a72b9fcf1ee
Galaxyben <galaxyben1@gmail.com>
2017-12-05 14:51:31 -0600

    Anexo Script PU pt1
    adas

---
+++ Assets/Scripts/PowerUp_Jugador.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp_Jugador : MonoBehaviour {
+	private MartCarMovement MCM;
+
+	// Use this for initialization
+	void Start () {
+		MCM.GetComponents<MartCarMovement> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider _col)
+	{
+
+	}
+}
