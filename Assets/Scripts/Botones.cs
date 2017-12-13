5176cddebdbb5ac6b7257bd0fbe30add89553fd6
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2017-12-13 12:33:51 -0600

    Super Mejora
    Esta Madre agrega animaciones y efectos a la camara

---
+++ Assets/Scripts/Botones.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class Botones : MonoBehaviour {
+
+	public void Salir()
+	{
+		Application.Quit();
+	}
+
+	public void Info()
+	{
+		
+	}
+
+	public void Comezar()
+	{
+		EditorSceneManager.LoadScene ("Seleccion_personaje");
+	}
+
+	public void Regresar()
+	{
+		EditorSceneManager.LoadScene ("Inicio");
+	}
+}
