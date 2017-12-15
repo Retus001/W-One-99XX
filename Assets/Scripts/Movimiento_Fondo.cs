6a8158b5d15fedb0b1e0cc615bd531fdeb50af6f
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2017-12-15 11:31:04 -0600

    No se que vergas hago
    XDXDXDXDXDXDXDXDXDXD

---
+++ Assets/Scripts/Movimiento_Fondo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+
+public class Movimiento_Fondo : MonoBehaviour {
+
+	void Start()
+	{
+		Invoke ("CargarOtraScena", 5f);
+	}
+
+	void CargarOtraScena()
+	{
+		EditorSceneManager.LoadScene ();
+	}
---
+++ Assets/Scripts/Selecion_de_personajes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.UI;
+
+public class Selecion_de_personajes : MonoBehaviour {
+
+	public Button[] Opciones;
+
+	public void Opcion1()
+	{
+		Opciones [1].enabled = false;
+	}
+
+	public void Opcion2()
+	{
+		
+	}
+
+	public void Opcion3()
+	{
+
+	}
+}
